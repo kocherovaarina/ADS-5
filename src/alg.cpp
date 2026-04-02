9f47e465ece9b4ff9f8c9d0bfbaa870361e01063
kocherovaarina <arinakocerova@gmail.com>
2026-04-02 11:55:53 +0300

    Task done by Kocherova Arina from 25 IST 4-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,68 @@
 // Copyright 2025 NNTU-CS
-#include <string>
-#include <map>
-#include "tstack.h"
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+#include <string>
+#include <stack>
+#include <cctype>
+#include <sstream>
+#include <map>
+
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
 }
 
-int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+std::string infx2pstfx(const std::string& inf) {
+    std::stack<char> ops;
+    std::string output;
+    for (size_t i = 0; i < inf.length(); ++i) {
+        char token = inf[i];
+        if (std::isalnum(token)) {
+            output += token;
+            output += ' ';
+        } else if (token == '+' || token == '-' || token == '*' || token == '/') {
+            while (!ops.empty() && precedence(ops.top()) >= precedence(token)) {
+                output += ops.top();
+                output += ' ';
+                ops.pop();
+            }
+            ops.push(token);
+        } else if (token == '(') {
+            ops.push(token);
+        } else if (token == ')') {
+            while (!ops.empty() && ops.top() != '(') {
+                output += ops.top();
+                output += ' ';
+                ops.pop();
+            }
+            ops.pop();
+        }
+    }
+    while (!ops.empty()) {
+        output += ops.top();
+        output += ' ';
+        ops.pop();
+    }
+    return output;
+}
+
+int eval(const std::string& post) {
+    std::stack<int> values;
+    std::istringstream iss(post);
+    std::string token;
+    while (iss >> token) {
+        if (std::isdigit(token[0])) {
+            values.push(std::stoi(token));
+        } else {
+            int right = values.top(); values.pop();
+            int left = values.top(); values.pop();
+            switch (token[0]) {
+                case '+': values.push(left + right); break;
+                case '-': values.push(left - right); break;
+                case '*': values.push(left * right); break;
+                case '/': values.push(left / right); break;
+            }
+        }
+    }
+    return values.top();
 }
